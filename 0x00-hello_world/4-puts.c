863aa1635418ce11e4eb08a5455d912a8dfeb4c0
uniquebee <112019444+uniquebee@users.noreply.github.com>
2022-09-08 15:09:59 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 /*
  * File: 4-puts.c
  * Auth: Brennan D Baraban <375@holbertonschool.com>
- *  */
+ */
 
 #include <stdio.h>
 
 /**
  * main - Prints "Programming is like building a multilingual
- * puzzle, followed by a new line.
+ *                puzzle, followed by a new line.
  *
  *  Return: Always 0.
  */
